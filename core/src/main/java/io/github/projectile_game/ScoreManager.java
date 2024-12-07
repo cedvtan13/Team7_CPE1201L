48597c95288500f3352aa29a994f55337768c52a
Kin Marius <23104608@usc.edu.ph>
2024-12-07 21:49:05 +0800

    Merge branch 'main' of https://github.com/cedvtan13/Team7_CPE1201L into updates
    # Conflicts:
    #	.gradle/8.10.2/executionHistory/executionHistory.bin
    #	.gradle/8.10.2/executionHistory/executionHistory.lock
    #	.gradle/8.10.2/fileHashes/fileHashes.bin
    #	.gradle/8.10.2/fileHashes/fileHashes.lock
    #	.gradle/8.10.2/fileHashes/resourceHashesCache.bin
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/workspace.xml
    #	core/build/libs/core-$projectVersion.jar
    #	core/build/tmp/compileJava/previous-compilation-data.bin

--- core/src/main/java/io/github/projectile_game/ScoreManager.java
+++ core/src/main/java/io/github/projectile_game/ScoreManager.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.utils.Align;
 public class ScoreManager {
     private int scorePlayer1 = 0;
     private int scorePlayer2 = 0;
-    private Label scoreLabel1, scoreLabel2;
+    private Label scoreLabel1, scoreLabel2, winLabel;
 
     private Skin skin;
 
@@ -19,40 +19,50 @@ public class ScoreManager {
     }
 
     public void updateScore(PlayerTurn currentPlayer, Body bodyA, Body bodyB, Ball ball1, Ball ball2) {
+
         // Only award score if it's the current player's turn
         if (currentPlayer == PlayerTurn.PLAYER1) {
             if (bodyA == ball2.getBody() || bodyB == ball2.getBody()) {
-                scorePlayer1++;  // Player 1 scores
-                scoreLabel1.setText("Player 1 Score: " + scorePlayer1);
+                scorePlayer2++;  // Player 1 scores
+                scoreLabel2.setText("Player 2 Score: " + scorePlayer2);
             }
         } else if (currentPlayer == PlayerTurn.PLAYER2) {
             if (bodyA == ball1.getBody() || bodyB == ball1.getBody()) {
-                scorePlayer2++;  // Player 2 scores
-                scoreLabel2.setText("Player 2 Score: " + scorePlayer2);
+                scorePlayer1++;  // Player 2 scores
+                scoreLabel1.setText("Player 1 Score: " + scorePlayer1);
             }
         }
-    }
-
-    public int getScore(PlayerTurn player) {
-        if (player == PlayerTurn.PLAYER1) {
-            return scorePlayer1;
-        } else if (player == PlayerTurn.PLAYER2) {
-            return scorePlayer2;
-        }
-        return 0;
     }
 
     // Set up score labels
     public void setUpScoreLabels(Stage stage) {
         scoreLabel1 = new Label("Player 1 Score: 0", skin);
-        scoreLabel1.setPosition(450, Gdx.graphics.getHeight() - 100);
+        scoreLabel1.setPosition(50, Gdx.graphics.getHeight() - 100);
         scoreLabel1.setAlignment(Align.left);
         stage.addActor(scoreLabel1);
 
         scoreLabel2 = new Label("Player 2 Score: 0", skin);
-        scoreLabel2.setPosition(50, Gdx.graphics.getHeight() - 100);
+        scoreLabel2.setPosition(450, Gdx.graphics.getHeight() - 100);
         scoreLabel2.setAlignment(Align.left);
         stage.addActor(scoreLabel2);
+    }
+
+    // Update score based on collision and current player
+
+
+    public void winCondition(int scorePlayer1, int scorePlayer2, Stage stage){
+        if(scorePlayer1 == 5){
+            winLabel = new Label("Player 1 Wins!", skin);
+            winLabel.setPosition(100, Gdx.graphics.getHeight() - 100);
+            winLabel.setAlignment(Align.left);
+            stage.addActor(winLabel);
+        }
+        else {
+            winLabel = new Label("Player 2 Wins!", skin);
+            winLabel.setPosition(100, Gdx.graphics.getHeight() - 100);
+            winLabel.setAlignment(Align.left);
+            stage.addActor(winLabel);
+        }
     }
 
     public void dispose() {
