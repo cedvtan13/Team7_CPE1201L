d122d523e8322b108bf923cdec6a28de6305469d
Cedric Vince M. Tan <21101045@usc.edu.ph>
2024-12-07 13:08:09 +0800

    Add files via upload

---
+++ lwjgl3/src/main/java/io/github/projectile_game/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,31 @@
+package io.github.projectile_game.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.github.projectile_game.GameApplication;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new GameApplication(), getDefaultConfiguration()); // Change Main to GameApplication
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("ProjectileGame");
+        // Vsync limits frames per second to what your hardware can display, and helps eliminate screen tearing.
+        configuration.useVsync(true);
+        // Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional refresh rates.
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        // Window size
+        configuration.setWindowedMode(800, 600); // Adjust the resolution as needed
+        // Icons
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
